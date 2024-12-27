ef0ca583993b1ddec518e5f0a7f4a769852cb596
vithom <vithom 74423292+vithom@users.noreply.github.com>
2024-12-27 12:45:14 +0100

    2024: update template with debug print function

--- 2024/template.py
+++ 2024/template.py
@@ -1,9 +1,17 @@
 import os
 
+# test for adding a global lib
+#
+# sys.path.append(os.path.join(os.getcwd(), 'mylib'))
+# from input import input, input_raw
+
 # file = os.path.dirname(__file__)+"/input_example.txt"
 file = os.path.dirname(__file__)+"/input.txt"
 
 debug = 0
+
+def dprint(*var):
+    if debug: print(*var)
 
 def input(trim = True):
     with open(file, 'rt') as f:
